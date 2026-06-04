03f4d9571adf0e7a0f69cc50ade8d36c86148c13
BenKoncsik <koncsik.benedek.andras@gmail.com>
2026-06-04 08:19:51 +0200

    Update

--- Avalonia/src/GitRunnerManager.App/BatteryMonitorFactory.cs
+++
@@ -1,9 +0,0 @@
-using GitRunnerManager.Core.Interfaces;
-using GitRunnerManager.Platform.Services;
-
-namespace GitRunnerManager.App;
-
-public class BatteryMonitorFactory : IBatteryMonitorFactory
-{
-    public IBatteryMonitor Create() => new BatteryMonitor();
-}
